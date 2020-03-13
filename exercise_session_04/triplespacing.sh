fa3576c20bb6e3f5ec93a296bc5ac079532563ca
NoraSalgo <nora.salgo@uzh.ch>
2020-03-13 17:36:42 +0100

    triplespacing.sh

---
+++ exercise_session_04/triplespacing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#IFS: Internal Field Separator
+#read: reads a single line from standard input, read considers a newline character as the end of a line
+echo "Filename: $1"
+
+if test -z "$1" 
+then
+      echo "no file given"
+fi
+
+if [ ! -f $1 ]; 
+then
+    echo "$1 does not exist"
+fi
+
+
+while IFS= read line; 
+do 
+	echo ${line}; 
+	printf '\n';
+	printf '\n'
+done < $1
